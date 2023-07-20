730d46e7122a50d04013c5b990ebd93810e26860
Igor Gabriel <igorgabsprofissional@gmail.com>
2023-07-20 20:25:36 -0300

    added: Implementação da aplicação em React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import logo from './logo-travelly.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='page'>
+
+      <div className='form-container'>
+
+        <h1><img id='logo-travelly' src={logo} alt='logo'/></h1>
+
+        <h2>Descubra o motivo de viagens por período</h2>
+
+        <label className="text-input" htmlFor="origin">Aeroporto de Partida (Código do Aeroporto):</label>
+        <input type="text" id="origin" placeholder="Código do Aeroporto de Destino" required></input>
+
+        <label className="text-input" htmlFor="destination">Aeroporto de Destino (Código do Aeroporto):</label>
+        <input type="text" id="destination" placeholder="Código do Aeroporto de Destino" required></input>
+        
+        <label className="text-input" htmlFor="departure-date">Data de Partida:</label>
+        <input type="date" id="departure-date" required></input>
+
+        <label className="text-input" htmlFor="return-date">Data de Retorno:</label>
+        <input type="date" id="return-date" required></input>
+        
+        <button id="search-btn" onClick={1 + 1}>Pesquisar</button>
+
+      </div>
     </div>
   );
 }
---
+++ src/Modal.js
@@ -0,0 +1,27 @@
+import './Modal.css'
+
+function Modal (){
+
+    return(
+
+    <div id="modal" className="modal">
+
+        <div className="modal-content">
+
+            <span className="close">&times;</span>
+
+            <div className="showDisplay">
+                <p>
+                    Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.
+                </p>
+            </div>
+
+            <button id="close-modal-btn">Fechar</button>
+
+        </div>
+    </div>
+    )
+
+}
+
+export default Modal;
